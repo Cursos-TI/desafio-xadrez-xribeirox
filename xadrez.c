3edefe5f03d08624417fccbfd36a8435a2b0fd9b
xribeirox <avancedesignstudio@gmail.com>
2025-03-26 00:55:25 +0000

    nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -1,15 +1,64 @@
 #include <stdio.h>
 
-void mover_torre(int vezes) {
+//CHAMADAS RECURSIVO
+//torre recursive
+void movertorre(int vezes) {
     if (vezes == 0) {
         return;
     }
-    
     printf("Direita\n");
-    mover_torre(vezes - 1);
+    movertorre(vezes - 1);
 }
 
+//bispo recursive
+void imprimirdireita(int casas) {
+    if (casas > 0) {
+        printf("Direita\n");
+        imprimirdireita(casas - 1);
+    }
+}
+
+//rainha recursive
+void moverrainha(int vezes) {
+    if (vezes == 0) {
+        return;
+    }
+    printf("Esquerda\n");
+    moverrainha(vezes - 1);
+}
+
+//FIM DOS VOIDS
+
 int main() {
-    mover_torre(5);
+    //Torre (5 casas para a direita) - for
+    printf("Movimento da Torre:\n");
+    movertorre(5);
+
+    //Bispo (5 casas na diagonal para cima e direita) - loop while
+    printf("\nMovimento do Bispo:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Cima, ");
+        imprimirdireita(1);
+    }
+
+    //Rainha (8 casas para a esquerda) - loop do-while
+    printf("\nMovimento da Rainha:\n");
+    moverrainha(8);
+
+    //CAVALO  (2 casas pra cima e 1 direita) - aninhado
+    printf("\n Movimento do Cavalo:\n");
+
+    for (int i = 0; i < 5; i++) {  // Loop externo (5 vezes)
+        
+        for (int j = 0; j < 2; j++) {  // Loop interno (duas repetições para "Cima")
+            printf("Cima, ");
+            
+            if (j == 1) {  // Após duas subidas, o cavalo move para a direita
+                printf("Direita\n");
+                break;
+            }
+        }
+    }
     return 0;
 }
+
